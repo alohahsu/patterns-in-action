0eb8e174cee42ecc8bc27bfcf0312bc13cd5e3d2
AlohaHsu <bostonhsu@icloud.com>
2021-10-14 10:04:02 +0800

    I have found a Germany hotel.

--- src/cn/windssoft/patterns/structure/Test.java
+++ src/cn/windssoft/patterns/structure/Test.java
@@ -3,18 +3,11 @@ package cn.windssoft.patterns.structure;
 public class Test {
 
 	public static void main(String[] args) {
-
-		//初始化一个德国插座对象， 用一个德标接口引用它
-		DBSocketInterface dbSocket = new DBSocket();
-
-		//创建一个旅馆对象
-		Hotel hotel = new Hotel(dbSocket);
-
-		//在旅馆中给手机充电
-		hotel.charge();
-
 		//我带着一个国标充电器到了德国
 		GBSocketInterface gbSocket = new GBSocket();
+
+		//我发现了一个德国旅店
+		Hotel hotel = new Hotel();
 
 	}
 }
