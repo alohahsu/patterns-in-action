5ad621a8bd095a3ef08ae0e8e344437a7bb90dee
AlohaHsu <bostonhsu@icloud.com>
2021-10-14 11:33:27 +0800

    总结

--- src/cn/windssoft/patterns/structure/SocketAdapter.java
+++ src/cn/windssoft/patterns/structure/SocketAdapter.java
@@ -39,10 +39,11 @@ public class SocketAdapter implements DBSocketInterface{   //实现旧接口
 	//如果想连接系统1和系统3，则要构造一个适配器：}---<
 	//匹配：3333}}----<<1111
 
-	//适配有方向性。
-	//适配的入向是座系统
+	//适配有方向性
+	//适配的去向是座系统
 	//适配的来向是插系统
 	//适配不能反插
+	//持有一个类的实例成员，就意味着，这个地方可以被插，这个地方是个槽。
 
 	//组合新接口
 	private GBSocketInterface gbSocket;
