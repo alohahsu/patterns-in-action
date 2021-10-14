aa9e30519baa77aa71993c4be66bbdf6e7db2dad
AlohaHsu <bostonhsu@icloud.com>
2021-10-14 09:52:48 +0800

    Init Proj.

--- src/cn/windssoft/patterns/structure/DBSocket.java
+++ src/cn/windssoft/patterns/structure/DBSocket.java
@@ -3,7 +3,7 @@ package cn.windssoft.patterns.structure;
 /**
  * 德国插座
  */
-public class DBSocket {
+public class DBSocket implements DBSocketInterface {
 	public void powerWithTwoRound(){
 		System.out.println("使用两项圆头的插孔供电");
 	}
